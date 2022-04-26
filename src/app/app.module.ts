f3a864572da453171babd29cd9401cf2ad03ca32
RusyaDeWitt <kashayevruslan@gmail.com>
2022-04-27 01:12:18 +0600

    Changed File System

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPage } from './LoginPage/loginpage.component';
+import { HomePage } from './HomePage/homepage.component';
+import { TopBar } from './TopBar/topbar.component';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    HomePage,
+    TopBar
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: 'login', component: LoginPage },
+      { path: 'home', component: HomePage },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
